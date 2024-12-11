b8348a79d98342906ab382682a884cffa3202ac0
pjeov <trimeldawanim@gmail.com>
2024-12-11 16:12:56 +0700

    view 1 dan 2

--- view 1.sql
+++ view 1.sql
@@ -1,4 +1,6 @@
+create view nama_pemesan as
 SELECT        Detail_Transaksi.id_pemesanan, Pelanggan.nama
 FROM            Pelanggan INNER JOIN
                          Pemesanan ON Pelanggan.id_pelanggan = Pemesanan.id_pelanggan INNER JOIN
                          Detail_Transaksi ON Pemesanan.id_pemesanan = Detail_Transaksi.id_pemesanan
+				
--- view 2.sql
+++ view 2.sql
@@ -1,7 +1,6 @@
-SELECT        Transaksi.id_transaksi, Transaksi.metode_pembayaran, Pengiriman.id_pengiriman
-FROM            Karyawan INNER JOIN
-                         Karyawan_Produk ON Karyawan.id_karyawan = Karyawan_Produk.id_karyawan INNER JOIN
-                         Produk ON Karyawan_Produk.id_produk = Produk.id_produk CROSS JOIN
-                         Transaksi INNER JOIN
-                         Pengiriman ON Transaksi.id_transaksi = Pengiriman.id_transaksi CROSS JOIN
-                         Pelanggan
+create view Karyawan_pemesanan as
+SELECT Karyawan_Produk.id_detailtransaksi, Karyawan.nama, Produk.nama_produk, Produk.harga_beli, Produk.harga_jual, Pemesanan.status
+FROM     Karyawan_Produk INNER JOIN
+                  Karyawan ON Karyawan_Produk.id_karyawan = Karyawan.id_karyawan INNER JOIN
+                  Produk ON Karyawan_Produk.id_produk = Produk.id_produk CROSS JOIN
+                  Pemesanan
