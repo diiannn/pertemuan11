c5031a1b4449e8810755e40712f412f323ef84f5
diiannn <dianastri75@gmail.com>
2024-12-11 16:21:24 +0700

    commit view 3

--- view 3.sql
+++ view 3.sql
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-CREATE VIEW Pengiriman_Kurir AS
-SELECT 
-    id_pengiriman,
-    alamat_pengiriman,
-    kurir,
-    status_pengiriman
-FROM 
-    Pengiriman;
-=======
+use jastip2
 CREATE VIEW total_pembayaran AS
 SELECT t.id_transaksi, t.tgl_transaksi, SUM(d.Total_harga) AS total_pembayaran
 FROM Transaksi t
 JOIN Detail_Transaksi d ON t.id_transaksi = d.id_detailtransaksi
 GROUP BY t.id_transaksi, t.tgl_transaksi;
->>>>>>> 84dcf0fab7aed522bc27ae5380c4080576f10790
